0f39441f608465dcce887b42ddc4fc2ad23f83d5
ahmar-js <67374904+ahmar-js@users.noreply.github.com>
2022-10-08 17:59:46 +0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# Created by Ahmer Aamir
+# Created on 7 Oct 2022
+
+fibRange = int(input('Enter Range of Fibonacci '))
+
+
+def generate_fibonacci(n):
+    num1, num2 = 0, 1
+    for i in range(n):
+        num1, num2 = num2, num1 + num2
+        yield num1
+
+
+print(list(generate_fibonacci(fibRange)))
